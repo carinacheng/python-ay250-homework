27250bcb530c1cc2f5d26b82999b189184a2e583
carinacheng <ccheng@berkeley.edu>
2018-05-04 16:30:47 -0700

    -Added code to tally up lunch hosting counts -Added table that shows the counts -Edited a database entry that was missing a last name

--- final_project/app/forms.py
+++ final_project/app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 # Sign-Up Form consists of 3 string fields (name, email, date) and a submit field
 class SignUpForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
+    name = StringField('Name (please match to table on right, if applicable)', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired()])
     date = DateField('Date', validators=[DataRequired()], format="%m/%d/%Y")
     submit = SubmitField('Sign Up')
--- final_project/app/routes.py
+++ final_project/app/routes.py
@@ -5,6 +5,7 @@ from app.models import User
 from app import db
 import datetime
 import numpy as np
+import collections
 
 # Function to parse database information
 def parse_db(db):
@@ -32,8 +33,27 @@ def parse_db(db):
         if diffs.mask.all() == False: # if there exists a future date 
             ind = np.argmin(diffs) # index of minimum difference - this is the closest upcoming date 
             items[ind]['flag'] = True # set flag to True to mark this upcoming date
-
     return items
+
+
+def parse_counts(items):
+    """
+    Counts up the number of times each person has hosted lunch.
+    Input: list of items from parse_db function
+    Output: dictionary with keys "names" and values "counts"
+    """
+    names = ['Adrian Liu', 'Carina Cheng', 'Zaki Ali', 'Morgan Presley', 'Josh Dillon', 'Nipanjana Patra', 
+            'Nick Kern', 'Deepthi Gorthi', 'Jack Hickish', 'Kara Kundert', 'Phil Bull', 'Avery Kim'] # people who should be in the lunch-buying rotation
+    names_in_db = [item['name'] for item in items] # people who have signed up in the database
+    counts = {}
+    for name in names: counts[name] = 0 # make dictionary where everyone has a count of 0
+    for name in names_in_db: # loop through database entries
+        if name in names: # if the name is already in the counts dictionary XXX names have to match exactly
+            counts[name] += 1 # increase count
+        else: counts[name] = 1 # if newcomer isn't in the original list, add them and give a count of 1
+    counts = collections.OrderedDict(sorted(counts.items())) # order alphabetically
+    for name in counts: counts[name] = str(counts[name]) # convert integers into strings
+    return counts
 
 
 # Main webpage
@@ -42,6 +62,7 @@ def parse_db(db):
 def index():
     form = SignUpForm() # sign-up form
     items = parse_db(db) # database information
+    counts = parse_counts(items) # count information
     if form.validate_on_submit(): # add user to database
         info = User(name=str(form.name.data), email=str(form.email.data), date=form.date.data.strftime("%m/%d/%Y"))
         dates = [user.date for user in User.query.all()] # dates already in database
@@ -52,7 +73,7 @@ def index():
         db.session.commit() # add information that's submitted into database
         flash('{}, you are now signed-up to provide lab Friday lunch!'.format(form.name.data)) # flash a message on the screen
         return redirect(url_for('index')) # return home
-    return render_template('index.html', form=form, items=items) # render page
+    return render_template('index.html', form=form, items=items, counts=counts) # render page
 
 # Instructions page
 @app.route('/instructions', methods=['GET'])
